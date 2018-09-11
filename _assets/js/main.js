a01d727868c0836a7738a9823be0c48b469892e8
Aleksandr Ljamin <aleksandr.lmn@gmail.com>
2018-09-12 00:04:16 +0930

    Revert "commiting to resolve merge"
    This reverts commit 68e4fddea83cd176ce834c8ea85b209a73d1b12b, reversing
    changes made to c96665030f5ca2cb782281074fb1cc1e31f75314.

---
+++ _assets/js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+    var $posts = $('.posts > article');
+    
+    $posts.each(function() {
+
+        var $this = $(this),
+            $image = $this.find('.image'), 
+            $img = $image.find('img'),
+            $link = $this.find('.link'),
+            x;
+
+        // Set image.
+            $this.css('background-image', 'url(' + $img.attr('src') + ')');
+
+        // Set position.
+            if (x = $img.data('position'))
+                $image.css('background-position', x);
+
+        // Hide original.
+            $image.hide();
+
+    });
+});
